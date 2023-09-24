cc745ab4089a2bbca2990e1fb7da720c444d0f87
unknown <lutfiemreokcularli@gmail.com>
2023-09-24 15:40:30 +0300

    elemanlar arasında gezinti

---
+++ yeniDersler(dom_icerir)/gezinti.js
@@ -0,0 +1,14 @@
+let deger;
+
+const ikinciSehirler = document.querySelector('ul.cliste2');
+console.log(ikinciSehirler);
+
+deger = ikinciSehirler.childNodes;
+deger = ikinciSehirler.childNodes[1].nodeType;
+
+deger = ikinciSehirler.firstElementChild
+
+deger = ikinciSehirler.parentElement;
+deger = document.querySelector('li').nextElementSibling.nextElementSibling;
+
+console.log(deger)
