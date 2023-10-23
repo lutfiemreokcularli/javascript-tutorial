e4364ef493e7d886133212a96634ea4be0e61da6
unknown <lutfiemreokcularli@gmail.com>
2023-10-23 17:23:28 +0300

    23 Ekim

--- not sepeti/not_sepeti.js
+++ not sepeti/not_sepeti.js
@@ -1,1 +1,50 @@
-console.log('buradayım')
+const yeniGorev = document.querySelector('.input-gorev');
+const yeniGorevEkleBtn = document.querySelector('.btn-gorev-ekle');
+const gorevListesi = document.querySelector('.gorev-listesi');
+const gorevEkle = (e) => {
+    e.preventDefault();
+    //div oluşturma
+    const gorevDiv = document.createElement('div');
+    gorevDiv.classList.add('gorev-item');
+
+    //li oluşturma
+    const gorevLi = document.createElement('li');
+    gorevLi.classList.add('gorev-tanim');
+    gorevLi.textContent = yeniGorev.value;
+    gorevDiv.appendChild(gorevLi);
+
+    //Görev Tamam button oluşturma
+    const gorevOKButton = document.createElement('button');
+    gorevOKButton.classList.add('gorev-btn');
+    gorevOKButton.classList.add('gorev-btn-tamamlandi');
+    gorevOKButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    gorevDiv.appendChild(gorevOKButton);
+
+    //Görev Sil button oluşturma
+    const gorevSilButton = document.createElement('button');
+    gorevSilButton.classList.add('gorev-btn');
+    gorevSilButton.classList.add('gorev-btn-sil');
+    gorevSilButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    gorevDiv.appendChild(gorevSilButton);
+
+    yeniGorev.value = '';
+    //ul ye div ekleme
+    gorevListesi.appendChild(gorevDiv);
+}
+const gorevSilveTamamla = (e) => {
+    //console.log(e.target);
+    const tiklanilanEleman = e.target;
+    if(tiklanilanEleman.classList.contains('gorev-btn-tamamlandi')){
+        console.log('checked tıklandı');
+        tiklanilanEleman.parentElement.classList.toggle('gorev-tamamlandi');
+    }else if(tiklanilanEleman.classList.contains('gorev-btn-sil')){
+        tiklanilanEleman.parentElement.classList.add('kaybol');
+        tiklanilanEleman.parentElement.addEventListener('transitionend',function(){
+            tiklanilanEleman.parentElement.remove();
+        });
+        
+    }
+
+}
+yeniGorevEkleBtn.addEventListener('click',gorevEkle);
+gorevListesi.addEventListener('click',gorevSilveTamamla);
--- objects/objects.js
+++ objects/objects.js
@@ -1,5 +1,5 @@
 //factory fonksiyonlar
-const factoryFunc = (ad,yas,evliMi,okul) =>{
+const factoryFunc = (ad,yas,evliMi,okul,username) =>{
     return {
         isim:ad,
         yas:yas,
@@ -7,11 +7,12 @@ const factoryFunc = (ad,yas,evliMi,okul) =>{
         okul:okul,
         bilgileriSoyle: function (){
             return `adı :  ${this.isim}\n
-            yaşı: ${this.yas}\n evlilik durumu: ${this.evliMi}`;
-        } 
+            yaşı: ${this.yas}\n evlilik durumu: ${this.evliMi} kullanıcı adı : ${this["user-name"]}`;
+        },
+        ['user-name']:username
     };
 }
-const emre = factoryFunc('emre','33','hayır','deu');
+const emre = factoryFunc('emre','33','hayır','deu','emreok');
 console.log(emre.bilgileriSoyle());
 
 //constructor functions
@@ -32,6 +33,7 @@ console.log(emre2.bilgileriSoyle())
 
 
 //constructor functions with prototype
+//prototype: değerlerinin aynı olduğunu bildiğimiz durumlarda kullanıyoruz.
 User.prototype.il = 'izmir';
 function User(ad,soyad){
     this.ad = ad;
@@ -39,3 +41,58 @@ function User(ad,soyad){
 }
 const user1 = new User('emre','ok');
 console.log(user1.il)
+
+
+//constructor functions with protptype 2
+
+function Ogrenci2(ad,soyad,okul,kullaniciAdi){
+    this.ad = ad;
+    this.soyad = soyad;
+    this.okul = okul;
+    this.username = kullaniciAdi;
+}
+Ogrenci2.prototype.bilgiVer = function (){
+    console.log(`merhaba benim adım ${this.ad} , soyadım ${this.soyad}, ve okulum ${this.okul}`);
+}
+
+const ogrNeew = new Ogrenci2('emre','okcu','deu','emreok');
+ogrNeew.bilgiVer();
+
+const ogrNeew2 = new Ogrenci2('demre','okcu','deu','emreok');
+ogrNeew2.bilgiVer();
+
+console.log(ogrNeew2.constructor.prototype)
+
+// factory fonksiyonlarda prototype kavramı yok.
+
+/* function OgrenciFactory2(name,surname,school,age){
+    return {
+        name:name,
+        surname,surname,
+        school:school,
+        ["age-age"]: age,
+    };
+}
+OgrenciFactory2.prototype.bilgileriSoyle = function (){
+    return `merhaba ben ${this.name} ve yasım ${this["age-age"]}`;
+ };
+const oFak = OgrenciFactory2('emre','okcu','deu',33);
+oFak.
+console.log(oFak.bilgileriSoyle()) */
+
+let dizi = [1,2,3]
+console.log(dizi.constructor.prototype)
+
+const isim = 'emre';
+console.log(isim instanceof Object);
+
+console.log(isim.constructor.prototype)
+
+const isis = 'emre'
+console.log(typeof isis);
+
+const sayi = new Number(1990);
+console.log(typeof sayi);
+sayi.text = 'bu kimin sayisi';
+console.log(sayi.text);
+console.log(sayi.constructor.prototype)
